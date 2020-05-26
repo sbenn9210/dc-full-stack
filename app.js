bc7601a6f2cd7b9e5bac6c92ae256069707c0b04
Shawn Benny <sbenn9210@gmail.com>
2020-05-26 08:49:38 -0500

    Add knex connect to database and database node connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./lib/db");
 
 const app = express();
 
@@ -18,11 +19,42 @@ const dummyLists = [
     name: "Todo list",
   },
 ];
+const dummyItems = [
+  {
+    uuid: "adsfagg",
+    description: "Wash the dog",
+    display_order: 1,
+  },
+  {
+    uuid: "pwprperwer",
+    description: "Wash the car",
+    display_order: 2,
+  },
+];
 
+//the homepage shows your lists
 app.get("/", (req, res) => {
-  res.render("index", { lists: dummyLists });
+  db.getLists().then((lists) => {
+    res.render("index", { lists: lists });
+  });
 });
 
-app.listen(port, () => {
-  console.log("express is listening on port " + port);
+// the list page shows the items in the list
+
+app.get("/list/:listUUID", (req, res) => {
+  res.render("list_page", { listName: "Dummy List", items: dummyItems });
 });
+
+const startExpressApp = () => {
+  app.listen(port, () => {
+    console.log("express is listening on port " + port);
+  });
+};
+
+const bootupSequenceFailed = (err) => {
+  console.error("Unable to connect to the database: ", err);
+  process.exit(1);
+};
+
+// global kickoff point
+db.connect().then(startExpressApp).catch(bootupSequenceFailed);
